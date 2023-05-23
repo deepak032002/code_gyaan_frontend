3ffed59a6f36828a218425ec0ecf7bd6e9cd8089
Deepak <deepakv032002@gmail.com>
2023-05-23 23:14:28 +0530

    add robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+
+    sitemap: "https://codegyaan.com/sitmap.xml",
+  };
+}
